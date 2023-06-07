f9d8e82332215505b80e9e7cee93d394f25a5172
Aw3som3ss <jabwunza@gmail.com>
2023-06-07 12:36:08 +0300

    Validation errors

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const connect = require('./config/db');
 const { register, loginuser } = require('./controllers/user_controllers');
 const User = require('./models//users_models');
 const bcrypt = require('bcrypt');
+const { body, validationResult } = require('express-validator');
 
 app.use(express.json());
 
@@ -13,46 +14,64 @@ app.get('/home', (req, res) => {
 
 const auth = async (req, res, next) => {
   try {
-
-  } catch (error) {
-
-  }
+  } catch (error) {}
   next();
 };
 
 app.post('/login', async (req, res) => {
   try {
-    let user = await User.findOne({ email:req.body.email });
-    if (!user) {
-      res.send('Oooops, user not registered!');
-    }
-   
     const compare = bcrypt.compareSync(req.body.password, user.password);
 
     if (!compare) {
-     res.send('Password is incorrect!');
+      res.send('Password is incorrect!');
     }
 
     // console.log('compare', compare);
     return res.send('Logged in successfully!');
-    
   } catch (error) {
     return res.status(400).send({ error: error.message });
   }
 });
-app.post('/register', auth, async (req, res) => {
-  try {
-    let user = await User.create(req.body);
-    return res.status(200).send(user);
-  } catch (error) {
-    return res.status(400).send({ error: error.message });
-  }
-});
+app.post(
+  '/register',
+  body('name')
+    .not()
+    .isEmpty()
+    .withMessage('Name is required')
+    .isLength({ min: 4, max: 8 })
+    .withMessage(
+      'Name should have minimum of 4 letters and maximum of 8 letters'
+    ),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        let newError;
+        newError = errors.array().map((err) => {
+          return { key: err.param, message: err.msg };
+        });
+        return res.send({ errors: newError });
+      }
 
-app.listen(8080 , async () => {
+      let register = await User.findOneAndDelete({ email: req.body.email });
+
+      console.log(register);
+
+      if (register) {
+        return res.send('User is already registered!');
+      }
+      let user = await User.create(req.body);
+      return res.status(200).send(user);
+    } catch (error) {
+      return res.status(400).send({ error: error.message });
+    }
+  }
+);
+
+app.listen(8080, async () => {
   try {
     await connect();
-    console.log('I am able to listen on port 8080');
+    console.log('Wow, I am able to listen on port 8080');
   } catch (error) {
     console.log({ error: error.message });
   }
